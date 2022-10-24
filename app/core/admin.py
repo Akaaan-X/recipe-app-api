ece30fd8d5fd947d14a1cf7a1d736e1b32768c9e
xlx <lxia0026@student.monash.edu>
2022-10-24 21:12:58 +1100

    admin&API schema

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,48 @@
-from django.contrib import admin  # noqa
+"""
+Django admin custoomization.
+"""
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+# integrate Django translation system to automatically translate text
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users."""
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),  # None means no title
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),  # make the page neat and tidy
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            )
+
+        }),
+    )
+
+
+admin.site.register(models.User, UserAdmin)
