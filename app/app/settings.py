24427c0a9903cef0d87a4bac8340b1dd63688b45
xlx <lxia0026@student.monash.edu>
2022-10-29 17:40:51 +1100

    image api

--- app/app/settings.py
+++ app/app/settings.py
@@ -127,8 +127,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+STATIC_URL = '/static/static/'
+MEDIA_URL = '/static/media/'
 
 MEDIA_ROOT = '/vol/web/media'
 STATIC_ROOT = '/vol/web/static'
@@ -144,5 +144,5 @@ REST_FRAMEWORK = {
 }
 
 SPECTACULAR_SETTINGS = {
-    'COMPONENT_SPLIT_QEQUEST': True,
+    'COMPONENT_SPLIT_REQUEST': True,
 }
